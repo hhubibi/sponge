4c2cde6e1755b95d6eaed85019b94498d8631c2e
hhubibi <hxynobibi@gmail.com>
2022-11-04 14:01:18 +0800

    finish byte stream

--- libsponge/byte_stream.cc
+++ libsponge/byte_stream.cc
@@ -12,42 +12,76 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-ByteStream::ByteStream(const size_t capacity) { DUMMY_CODE(capacity); }
+ByteStream::ByteStream(const size_t capacity) : _capacity(capacity),
+            _first(capacity - 1), _buffer() {
+    _buffer.resize(capacity);
+}
 
 size_t ByteStream::write(const string &data) {
-    DUMMY_CODE(data);
-    return {};
+    size_t remaining = remaining_capacity();
+    size_t can_write = data.length() > remaining ? remaining : data.length();
+    int next_last = _plus_num(_last, static_cast<int>(can_write));
+    int end_pos = _minus_num(next_last, 1);
+    if (end_pos < _last) {
+        copy(data.begin(), data.begin() + _capacity - _last, _buffer.begin() + _last);
+        copy(data.begin() + _capacity - _last, data.begin() + can_write, _buffer.begin());
+    } else {
+        copy(data.begin(), data.begin() + can_write, _buffer.begin() + _last);
+    }
+    _last = next_last;
+    _total_write += can_write;
+    _size += can_write;
+    return can_write;
 }
 
 //! \param[in] len bytes will be copied from the output side of the buffer
 string ByteStream::peek_output(const size_t len) const {
-    DUMMY_CODE(len);
-    return {};
+    size_t can_peek = len > _size ? _size : len;
+    string out_buffer(can_peek, ' ');
+    int next_first = _plus_num(_first, static_cast<int>(can_peek));
+    int start_pos = _plus_num(_first, 1);
+    if (next_first < start_pos) {
+        copy(_buffer.begin() + start_pos, _buffer.end(), out_buffer.begin());
+        copy(_buffer.begin(), _buffer.begin() + next_first + 1, out_buffer.begin() + this->_capacity - start_pos);
+    } else {
+        copy(_buffer.begin() + start_pos, _buffer.begin() + next_first + 1, out_buffer.begin());
+    }
+    return out_buffer;
 }
 
-//! \param[in] len bytes will be removed from the output side of the buffer
-void ByteStream::pop_output(const size_t len) { DUMMY_CODE(len); }
+//! \param[in] len bytes will be removed from the output side of the _buffer
+void ByteStream::pop_output(const size_t len) {
+    size_t can_pop = len > _size ? _size : len;
+    _first = _plus_num(_first, static_cast<int>(can_pop));
+    _size -= can_pop;
+    _total_read += can_pop;
+}
 
 //! Read (i.e., copy and then pop) the next "len" bytes of the stream
 //! \param[in] len bytes will be popped and returned
 //! \returns a string
 std::string ByteStream::read(const size_t len) {
-    DUMMY_CODE(len);
-    return {};
+    string out_buffer = peek_output(len);
+    _first = _plus_num(_first, out_buffer.length());
+    _total_read += out_buffer.length();
+    _size -= out_buffer.length();
+    return out_buffer;
 }
 
-void ByteStream::end_input() {}
+void ByteStream::end_input() {
+    _eof = true;
+}
 
-bool ByteStream::input_ended() const { return {}; }
+bool ByteStream::input_ended() const { return _eof; }
 
-size_t ByteStream::buffer_size() const { return {}; }
+size_t ByteStream::buffer_size() const { return _size; }
 
-bool ByteStream::buffer_empty() const { return {}; }
+bool ByteStream::buffer_empty() const { return _size == 0; }
 
-bool ByteStream::eof() const { return false; }
+bool ByteStream::eof() const { return _eof && _size == 0; }
 
-size_t ByteStream::bytes_written() const { return {}; }
+size_t ByteStream::bytes_written() const { return _total_write; }
 
-size_t ByteStream::bytes_read() const { return {}; }
+size_t ByteStream::bytes_read() const { return _total_read; }
 
-size_t ByteStream::remaining_capacity() const { return {}; }
+size_t ByteStream::remaining_capacity() const { return _capacity - _size; }
