457514039eb68e48a17f1abe148167e6f5472c84
hhubibi <hxynobibi@gmail.com>
2022-11-09 21:22:02 +0800

    finish lab2

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -11,9 +11,35 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    if (!_syn && !seg.header().syn) {
+        return;
+    }
+    if (_fin && _reassembler.empty()) {
+        return;
+    }
+
+    WrappingInt32 n{0};
+    if (seg.header().syn) {
+        _syn = true;
+        _isn = seg.header().seqno;
+        n = _isn + 1;
+    } else {
+        n = seg.header().seqno;
+    }
+    
+    if (seg.header().fin) {
+        _fin = true;
+    }
+    uint64_t index = unwrap(n, _isn, static_cast<uint64_t>(stream_out().bytes_written()));
+    _reassembler.push_substring(seg.payload().copy(), index - 1, seg.header().fin);
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const {
+    if (!_syn) {
+        return {};
+    } else {
+        return wrap(static_cast<uint64_t>(stream_out().bytes_written()) + 1 + (_fin && _reassembler.empty()), _isn);
+    }
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const { return stream_out().bytes_read() + _capacity - stream_out().bytes_written(); }
--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,5 +1,5 @@
 #include "wrapping_integers.hh"
-
+#include <iostream>
 // Dummy implementation of a 32-bit wrapping integer
 
 // For Lab 2, please replace with a real implementation that passes the
@@ -14,8 +14,7 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    return isn + static_cast<uint32_t>(n);
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +28,9 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    int32_t dist = n - wrap(checkpoint, isn);
+    if (checkpoint <= UINT32_MAX && dist < 0) {
+        return static_cast<uint64_t>(static_cast<uint32_t>(checkpoint) + dist);
+    }
+    return checkpoint + dist;
 }
